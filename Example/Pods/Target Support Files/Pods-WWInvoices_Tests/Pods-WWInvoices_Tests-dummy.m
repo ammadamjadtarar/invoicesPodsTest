4c4e0e54289e4b311d4975acdc49b9652e761391
Ammad Amjad Tarar <ammad.amjad@wework.com>
2019-02-21 12:28:39 +0800

    Added lib

---
+++ Example/Pods/Target Support Files/Pods-WWInvoices_Tests/Pods-WWInvoices_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WWInvoices_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_WWInvoices_Tests
+@end
---
+++ Example/Pods/Target Support Files/WWInvoices/WWInvoices-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_WWInvoices : NSObject
+@end
+@implementation PodsDummy_WWInvoices
+@end
